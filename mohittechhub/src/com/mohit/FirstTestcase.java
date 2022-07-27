d01a00b90be29cb438b577f2236c65fa10cc6785
mohit <mk82058104@gmail.com>
2022-07-27 07:32:07 +0530

    second commit

--- mohittechhub/src/com/mohit/FirstTestcase.java
+++ mohittechhub/src/com/mohit/FirstTestcase.java
@@ -3,7 +3,8 @@ package com.mohit;
 public class FirstTestcase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("Test");
 
 	}
 
